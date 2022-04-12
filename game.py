bc3da50b3079223d3b936ac0b5343a9bbeab3614
Jay Baek <jbaek.01@gmail.com>
2022-04-12 08:43:48 -1000

    improve interface; add mult + sub

--- game.py
+++ game.py
@@ -7,23 +7,62 @@ def rand100s():
      while True:
         yield random.randint(1,100)
 
+print(f'\n---START MATH DRILLS---'
+      f'\nEnter any letter to exit')
+
 if __name__ == "__main__":
     num = rand100s()
+
+    pts = 0
+    qNum = 1
 
     while True:
         a = next(rand100s())
         b = next(rand100s())
 
-        guess = input(f'\n{a} + {b} = ')
+        answer = 999999
+        guess = 999999
 
-        if int(guess) == a+b:
-           print(f'Well done! The correct answer is {a+b}.')
-        else:
-           print(f'Your answer is incorrect. The correct answer is {a+b}.')
+        ch = random.choice(['add', 'sub', 'mult'])
+        # ch = random.choice(['add', 'sub', 'mult', 'div'])
+        
+        if ch == 'add':
+            guess = input(f'\n{qNum}.  {a} + {b} = ')
+            answer = a + b
+        elif ch == 'sub':
+            guess = input(f'\n{qNum}.  {a} - {b} = ')
+            answer = a - b
+        elif ch == 'mult':
+            guess = input(f'\n{qNum}.  {a} * {b} = ')
+            answer = a * b
+        # DIVISION NEEDS TO BE SIMPLER
+        # elif ch == 'div':
+        #     guess = input(f'\n{a} / {b} = ')
+        #     answer = a / b
 
-        again = input('\nAgain? (y/n): ')
-        if again == 'y':
-            continue
-        elif again == 'n':
-            print('\nSee you next time. Good-bye.\n')
+        try:
+            int(guess)
+        except ValueError:
+            pts = pts - 1
+            print(f'      The correct answer is {answer}.\n      Pts: {pts}')
+            print(f'\nOk, see you next time. Good-bye. ^__^')
             sys.exit()
+
+        # if guess == 'quit':
+        #     print(f'The correct answer is {answer}.')
+        #     print(f'\nOk, see you next time. Good-bye. ^__^')
+        #     sys.exit()
+        if int(guess) == answer:
+            pts = pts + 1
+            print(f'      True. The correct answer is {answer}.\n      Pts: {pts}')
+        else:
+            pts = pts - 1
+            print(f'      False. The correct answer is {answer}.\n      Pts: {pts}')
+        qNum = qNum + 1
+
+        # again = input('\nAgain? (y/n): ')
+        # if again == 'y':
+        #     continue
+        # elif again == 'n':
+        #     print('\nOk, see you next time. Good-bye.\n')
+        #     sys.exit()
